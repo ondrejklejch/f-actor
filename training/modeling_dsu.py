e0d5d23ccbc2bed31b37f116ea3558df90615dc5
Ondrej Klejch <ondrej.klejch@gmail.com>
2026-07-12 12:25:21 +0100

    Revert "total_loss is not divided by grad_acc_steps."

--- training/modeling_dsu.py
+++ training/modeling_dsu.py
@@ -238,7 +238,7 @@ class DSUModel(ModelInitializerLoader):
                     raise NotImplementedError
 
             if not inference and total_loss is not None:
-                total_loss = total_loss
+                total_loss = total_loss / self.grad_acc_steps
 
         return {
             "loss": total_loss,
